a8c66c48b48d3b7cb89d0f255aec71f21d0dc59b
tkizm1 <tkizm1@users.noreply.github.com>
2024-04-26 22:12:21 +0800

    fix http 403

--- main.go
+++ main.go
@@ -6,6 +6,38 @@ import (
 )
 
 func main() {
+	//jar := tls_client.NewCookieJar()
+	//options := []tls_client.HttpClientOption{
+	//	tls_client.WithTimeoutSeconds(30),
+	//	tls_client.WithClientProfile(profiles.Okhttp4Android13),
+	//	tls_client.WithNotFollowRedirects(),
+	//	tls_client.WithCookieJar(jar), // create cookieJar instance and pass it as argument
+	//}
+
+	//client, err := tls_client.NewHttpClient(tls_client.NewNoopLogger(), options...)
+	//if err != nil {
+	//	log.Println(err)
+	//	return
+	//}
+	//
+	//req, err := http.NewRequest(http.MethodGet, "https://api.groq.com/platform/v1/user/profile", nil)
+	//if err != nil {
+	//	log.Println(err)
+	//	return
+	//}
+	//
+
+	//println(client)
+	//
+	//resp, err := client.Do(req)
+	//if err != nil {
+	//	log.Println(err)
+	//	return
+	//}
+	//if resp.StatusCode != 200 {
+	//	println(resp.StatusCode)
+	//	log.Fatal("not valid response")
+	//}
 	// 初始化配置
 	initialize.InitConfig()
 	// 初始化缓存
